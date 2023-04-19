5e1fd40cfd20d13b4371127898815cd7bef69313
liz9922 <124145079+liz9922@users.noreply.github.com>
2023-04-19 21:48:53 +0200

    ok

---
+++ src/main/java/KeyReader.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class KeyReader {
+
+    Properties prop;
+
+    public KeyReader(String fileKey) {
+        prop = new Properties();
+        String userHome = System.getProperty("user.home");
+        try {
+            FileInputStream input = new FileInputStream(userHome + "/OneDrive/Dokument/APIkeys/" + fileKey + ".txt");
+            prop.load(input);
+        } catch (Exception e) {
+        }
+    }
+    public String getAPIKey(){return prop.getProperty("apiKey");}
+
+    public String getKey(String key){
+        return prop.getProperty(key);
+    }
+}
