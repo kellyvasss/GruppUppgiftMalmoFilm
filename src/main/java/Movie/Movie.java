6a4cfc11a67789d6db77e3e69ba6e08bbc81855d
94hansnu <124634856+94hansnu@users.noreply.github.com>
2023-04-20 12:06:39 +0200

    ---

--- src/main/java/Movie/Movie.java
+++ src/main/java/Movie/Movie.java
@@ -5,22 +5,8 @@ public class Movie {
     //Deklarera instansvariabler för att lagra information om en film
      String title;
      String year;
-     String rated;
-     String released;
-     String runtime;
      String genre;
      String director;
-     String writer;
-     String actors;
-     String plot;
-     String language;
-     String country;
-     String awards;
-     String poster;
-     String metascore;
-     String imdbRating;
-     String imdbVotes;
-     String imdbID;
      String type;
 
 
@@ -30,22 +16,8 @@ public class Movie {
         //Sätt värden för alla variabler
         this.title = title;
         this.year = year;
-        this.rated = rated;
-        this.released = released;
-        this.runtime = runtime;
         this.genre = genre;
         this.director = director;
-        this.writer = writer;
-        this.actors = actors;
-        this.plot = plot;
-        this.language = language;
-        this.country = country;
-        this.awards = awards;
-        this.poster = poster;
-        this.metascore = metascore;
-        this.imdbRating = imdbRating;
-        this.imdbVotes = imdbVotes;
-        this.imdbID = imdbID;
         this.type = type;
     }
 
@@ -66,30 +38,6 @@ public class Movie {
         this.year = year;
     }
 
-    public String getRated() {
-        return rated;
-    }
-
-    public void setRated(String rated) {
-        this.rated = rated;
-    }
-
-    public String getReleased() {
-        return released;
-    }
-
-    public void setReleased(String released) {
-        this.released = released;
-    }
-
-    public String getRuntime() {
-        return runtime;
-    }
-
-    public void setRuntime(String runtime) {
-        this.runtime = runtime;
-    }
-
     public String getGenre() {
         return genre;
     }
@@ -104,94 +52,6 @@ public class Movie {
 
     public void setDirector(String director) {
         this.director = director;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
-    public String getActors() {
-        return actors;
-    }
-
-    public void setActors(String actors) {
-        this.actors = actors;
-    }
-
-    public String getPlot() {
-        return plot;
-    }
-
-    public void setPlot(String plot) {
-        this.plot = plot;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getAwards() {
-        return awards;
-    }
-
-    public void setAwards(String awards) {
-        this.awards = awards;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
-    public String getMetascore() {
-        return metascore;
-    }
-
-    public void setMetascore(String metascore) {
-        this.metascore = metascore;
-    }
-
-    public String getImdbRating() {
-        return imdbRating;
-    }
-
-    public void setImdbRating(String imdbRating) {
-        this.imdbRating = imdbRating;
-    }
-
-    public String getImdbVotes() {
-        return imdbVotes;
-    }
-
-    public void setImdbVotes(String imdbVotes) {
-        this.imdbVotes = imdbVotes;
-    }
-
-    public String getImdbID() {
-        return imdbID;
-    }
-
-    public void setImdbID(String imdbID) {
-        this.imdbID = imdbID;
     }
 
     public String getType() {
