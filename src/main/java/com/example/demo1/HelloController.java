d51b21752098aa6dcebc5149c3069524bcc7925e
kellyvasss <99kelvas@gafe.molndal.se>
2023-04-20 12:16:51 +0200

    ---

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -41,11 +41,71 @@ public class HelloController {
         Image image1 = new Image("https://m.media-amazon.com/images/M/MV5BZGM5NjliODgtODVlOS00OWZmLWIzYzMtMTI2OWIzMTM1ZGRhXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg");
         image.setImage(image1);
     }
+    private String userSearch(TextField textField) {
+        try {
+            return textField.getText();
+        } catch (RuntimeException e) {
+        } return null;
+    }
     @FXML
     protected void onSearchClick() {
         userInputYear.setVisible(true);
         userInputNotYear.setVisible(true);
         result.setVisible(true);
+        String search = userSearch(userInputNotYear);
+        if (!search.isEmpty()) {
+            userInputYear.setVisible(true);
+            userInputNotYear.setVisible(true);
+            result.setVisible(true);
+
+            String userChoiseBox = choiceBoxChoice();
+
+            switch (userChoiseBox) {
+                case "Search on: Movie Title":
+
+
+                    break;
+                case "Search on: Actor":
+
+                    break;
+                case "Search on: Director":
+
+                    break;
+                case "Search on: Genre":
+
+                    break;
+                case "Search on: Year":
+
+                    break;
+                case "Search on: Movie Title and Year":
+
+                    break;
+                case "Show all: Movie Titles":
+
+                    break;
+                case "Show all: Movies by Actor":
+
+                    break;
+                case "Show all: Movies by Director":
+
+                    break;
+                case "Show all: Movies by Genre":
+
+                    break;
+                case "Show all: Movies by Year":
+
+                    break;
+                default:
+                    alert.setTitle("Note");
+                    alert.setHeaderText(null);
+                    alert.setContentText("You have to search on something.");
+            }
         moviePic();
     }
+
+}@FXML
+    private String choiceBoxChoice() {
+        return categories.getValue().toString();
+    }
+
 }
