4e718ba4627dc76769309a40b8be58593b5dae79
94hansnu <124634856+94hansnu@users.noreply.github.com>
2023-04-20 15:05:39 +0200

    -----

--- src/main/java/OMDB/OMDBApi.java
+++ src/main/java/OMDB/OMDBApi.java
@@ -1,13 +1,9 @@
-package OMDB;
-
-import Movie.Movie;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 
 public class OMDBApi {
     private final String apiKey;
@@ -16,49 +12,104 @@ public class OMDBApi {
         this.apiKey = apiKey;
     }
 
-    public Movie getMovie(String title) throws IOException {
-        // Konstruerar API-anropets URL med API-nyckeln och titeln på filmen som ska hämtas
-        String apiUrl = String.format("http://www.omdbapi.com/?apikey=%s&t=%s", apiKey, URLEncoder.encode(title, "UTF-8"));
-        URL url = new URL(apiUrl);
+    public Movie getMovie(String title) {
+        JSONObject json = searchTitle(title);
+        return createMovie(json);
+    }
 
-        // Öppnar en anslutning till API:et
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+    public Movie getMovie(String title, String year) {
+        JSONObject json = searchTitleAndYear(title, year);
+        return createMovie(json);
+    }
 
-        // Läser API:ets svar och bygger upp en sträng
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        String response = "";
-        while ((line = reader.readLine()) != null) {
-            response += line;
-        }
-        reader.close();
+    public Movie getMovie(String title, String year, String type) {
+        JSONObject json = searchTitleYearAndType(title, year, type);
+        return createMovie(json);
+    }
 
-        // Pararser API-svaret och extraherar filmens titel, år, genre, regissör och skådespelare
-        String[] parts = response.split(",");
-        String movieTitle = "";
-        int movieYear = 0;
-        String movieGenre = "";
-        String movieDirector = "";
-        String movieActors = "";
-        for (String part : parts) {
-            String[] keyValue = part.split(":");
-            if (keyValue.length > 1) {
-                String key = keyValue[0].trim().replaceAll("\"", "");
-                String value = keyValue[1].trim().replaceAll("\"", "");
-                if (key.equals("Title")) {
-                    movieTitle = value;
-                } else if (key.equals("Year")) {
-                    movieYear = Integer.parseInt(value);
-                } else if (key.equals("Genre")) {
-                    movieGenre = value;
-                } else if (key.equals("Director")) {
-                    movieDirector = value;
-                } else if (key.equals("Actors")) {
-                    movieActors = value;
-                }
+    private JSONObject searchTitle(String title) {
+        String apiUrl = "https://www.omdbapi.com/?apikey=" + apiKey + "&t=" + title;
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
             }
+            in.close();
+            return new JSONObject(response.toString());
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
+    }
+
+    private JSONObject searchTitleAndYear(String title, String year) {
+        String apiUrl = "https://www.omdbapi.com/?apikey=" + apiKey + "&t=" + title + "&y=" + year;
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return new JSONObject(response.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private JSONObject searchTitleYearAndType(String title, String year, String type) {
+        String apiUrl = "https://www.omdbapi.com/?apikey=" + apiKey + "&t=" + title + "&y=" + year + "&type=" + type;
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return new JSONObject(response.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Movie createMovie(JSONObject json) {
+        if (json == null) {
+            return null;
+        }
+
+        String title = json.optString("Title", "");
+        String year = json.optString("Year", "");
+        String director = json.optString("Director", "");
+        String plot = json.optString("Plot", "");
+        String posterUrl = json.optString("Poster", "");
+
+        return new Movie(title, year, director, plot, posterUrl);
     }
 }
