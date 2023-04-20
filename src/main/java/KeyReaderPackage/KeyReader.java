b82ea2d0880c087f1227e2ca9967dc38d089deb5
kellyvasss <99kelvas@gafe.molndal.se>
2023-04-20 15:51:43 +0200

    ---

--- src/main/java/KeyReaderPackage/KeyReader.java
+++ src/main/java/KeyReaderPackage/KeyReader.java
@@ -1,22 +1,24 @@
 package KeyReaderPackage;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
 public class KeyReader {
 
     Properties prop;
-    public KeyReader fromFile(String fileKey) {
+
+    public KeyReader(String fileKey) {
         prop = new Properties();
         String userHome = System.getProperty("user.home");
         try {
             FileInputStream input = new FileInputStream(userHome + "/OneDrive/Dokument/APIkeys/" + fileKey + ".txt");
             prop.load(input);
         } catch (Exception e) {
+
         }
 
     }
-
     public String getAPIKey(){
         return prop.getProperty("apiKey");
     }
